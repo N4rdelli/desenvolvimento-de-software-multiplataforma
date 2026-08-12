32f4288ab3b05af5f15f8c995ab2b402cf3ddd81
Anelize Nardelli <anelizenardelli.tech@gmail.com>
2026-08-12 08:27:35 -0300

    lab-web: adiciona método para buscar todas as tarefas no projeto To-Do.

--- quarto-semestre/laboratorio-de-desenvolvimento-web/to-do/back-end/controllers/TaskController.js
+++ quarto-semestre/laboratorio-de-desenvolvimento-web/to-do/back-end/controllers/TaskController.js
@@ -5,20 +5,32 @@ import Task from "../models/Task.js";
 
 import { Types } from 'mongoose';
 
-export default class TarefaController{
-    static async Create(request, response){
-        // Requisição do tipo POST, que cria uma nova tarefa no banco de dados
-        
+export default class TarefaController {
+    // Todos os métodos que interagem com o banco de dados devem ser assíncronos (async/await), para não travar a aplicação
+    // Todos os métodos assíncronos devem ser tratados com try/catch, para capturar possíveis erros
+    
+    // Requisção do tipo GET, que busca todas as tarefas no banco de dados
+    static async GetAll(request, response) {
+        try {
+            const tasks = await Task.find();
+            return response.status(200).json({ message: "Tarefas encontradas com sucesso!", tasks });
+        } catch (error){
+            return response.status(500).json({ message: "Erro ao buscar tarefas.", error });
+        }
+    }
+
+    // Requisição do tipo POST, que cria uma nova tarefa no banco de dados
+    static async Create(request, response) {
         // Puxa os dados da nossa Tarefa do corpo da requisição
-        const {taskTitle, taskDescription, taskFinishDate, taskStatus} = request.body;
-        
+        const { taskTitle, taskDescription, taskFinishDate, taskStatus } = request.body;
+
         // Valida se todos os campos foram preenchidos. Podemos fazer outras validações depois.
         // Devemos fazer a validação tanto aqui no back-end quando no front-end
         if (!taskTitle || !taskDescription || !taskFinishDate || !taskStatus) {
-            return response.status(422).json({message: "Preencha todos os campos para prosseguir."});
+            return response.status(422).json({ message: "Preencha todos os campos para prosseguir." });
         }
 
-        // 
+        // Tenta criar a tarefa no banco de dados. Caso dê erro, o catch captura e retorna para o front-end
         try {
             const task = new Task({
                 title: taskTitle,
@@ -27,11 +39,10 @@ export default class TarefaController{
                 status: taskStatus,
             });
             const newTask = await task.save();
-            response.status(201).json({message: "Tarefa registrada com sucesso!", newTask});
-            return;
+            return response.status(201).json({ message: "Tarefa registrada com sucesso!", newTask });
 
-        } catch (error){
-            response.status(500).json({message: "Erro ao registrar a tarefa.", error});
+        } catch (error) {
+            return response.status(500).json({ message: "Erro ao registrar a tarefa.", error });
         }
     }
 }
